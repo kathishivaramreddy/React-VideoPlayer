dbea95c9dc78b84beea6897507fa01daef41c998
kathishivaramreddy <kathishivaram@gmail.com>
2018-09-08 14:34:38 +0530

    Passing the chooseVideo Functionality to Menu and Creatiing handleChange functionality in the Menu

--- src/Menu.js
+++ src/Menu.js
@@ -2,15 +2,22 @@ import React from 'react';
 
 export class Menu extends React.Component{
 
+  constructor(props){
+    super(props);
+    this.handleChange =this.handleChange.bind(this);  
+  }
+  handleChange(e){
+    const name=e.target.value
+    this.props.chooseVideo(name)
+  }
   render(){
     return(
-
-        <div>
+        <form onChange={this.handleChange}>
           <input type="radio" name="src" value="fast"/>fast
           <input type="radio" name="src" value="slow" />slow
           <input type="radio" name="src" value="cute" /> cute
           <input type="radio" name="src" value="eek" /> eek
-          </div>
+        </form>
       );
   }
 }
