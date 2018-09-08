68866792ddce71173e7c57bca3aab45a714f4a1a
kathishivaramreddy <kathishivaram@gmail.com>
2018-09-08 13:38:57 +0530

    adding heading video player

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = {src = VIDEOS.fast}
+    this.state = {src : VIDEOS.fast}
   }
 
   render() {
@@ -22,11 +22,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Video Player</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
